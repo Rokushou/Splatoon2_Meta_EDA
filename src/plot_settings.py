10a547acdac67ded2b07c33f711dc04c1ead172f
Herbert Yuan <rokusho1770@gmail.com>
2019-05-28 15:40:29 -0700

    init

---
+++ src/plot_settings.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+
+plt.style.use('seaborn-pastel')
+%matplotlib inline
+
+twi = '#DCB8E7' #Pale, light grayish mulberry
+twi_blu = '#273873' #Dark sapphire blue
+twi_pur = '#662D8A' #Moderate purple
+twi_pnk = '#ED438D' #Brilliant raspberry
