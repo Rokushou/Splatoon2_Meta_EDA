c5c1edea9b0cd134d185fbd81f7d61864b0c25c3
Herbert Yuan <rokusho1770@gmail.com>
2019-05-30 14:25:08 -0700

    color

--- src/plot_settings.py
+++ src/plot_settings.py
@@ -4,10 +4,26 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import seaborn as sns
 
-plt.style.use('seaborn-pastel')
+plt.style.use('seaborn')
 %matplotlib inline
 
-twi = '#DCB8E7' #Pale, light grayish mulberry
-twi_blu = '#273873' #Dark sapphire blue
-twi_pur = '#662D8A' #Moderate purple
-twi_pnk = '#ED438D' #Brilliant raspberry
+pink = '#fe447d'
+orange = '#f78f2e'
+orange_y = '#fedc0c'
+lime = '#d1f20a'
+emerald = '#5cd05b'
+teal = '#03c1cd'
+blue = '#0e10e6'
+violet = '#9208e7'
+orange_r = '#f84c00'
+yellow = '#f3f354'
+mint = '#bff1e5'
+green = '#3bc335'
+sea = '#7af5ca'
+blue_l = '#448bff'
+blue_d = '#101ab3'
+fuchsia = '#d645c8'
+green_n = '#0afe15'
+cyan = '#0acdfe'
+orange_n = '#ff9600'
+fuchsia_d = '#b21ca1'
